b9d2c2888ddd13b05f59aa6a82e3f852678c1450
wenlong <754563544@qq.com>
2021-07-14 14:21:05 +0800

    稍作调整

--- src/components/content/Bottom.vue
+++ src/components/content/Bottom.vue
@@ -6,18 +6,23 @@
         <span class="top"
           >岁月很长，人海茫茫，别回头也别将就，自然会有良人来爱你。</span
         >
-        <br>
+        <br />
         <div class="ddicon"></div>
         <span class="top"
           >惊喜是一件没有预期的礼物。在时间的沉淀下，在你的成长中，它总会到来，现在我们做的一切只为让惊喜更好地降临。</span
         >
-        <br>
+        <br />
         <div class="ddicon"></div>
         <span class="top"
           >没有完美无缺的东西，没有尽善尽美的人，关键是知道到底想要什么，想得到什么，假如什么都想要，结果只会什么都得不到。</span
         >
       </div>
       <span class="bottom">版权所有©13084991856@163.com 黄先森设计。</span>
+      <span class="bottom1"
+        >备案号：<a href="https://beian.miit.gov.cn/" target="_blank">
+          桂ICP备2021005613号</a
+        ></span
+      >
     </div>
   </footer>
 </template>
@@ -46,6 +51,17 @@ footer {
       color: rgba(255, 255, 255, 0.3);
       font-size: 12px;
       padding: 15px 0;
+    }
+    .bottom1 {
+      position: absolute;
+      bottom: 0px;
+      right: 200px;
+      color: rgba(255, 255, 255, 0.3);
+      font-size: 12px;
+      padding: 15px 0;
+      a {
+        color: #ccc;
+      }
     }
 
     .ddson {
